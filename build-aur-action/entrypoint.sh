3e4f9a481877159c4537643e1c6149910c37e4c8
咕咕 <xcz2034830996@163.com>
2024-02-15 13:10:58 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -3,17 +3,17 @@
 build() {
     HASH=$(git rev-parse --short HEAD)
     ID="$1@$HASH"
-    if grep -q "$ID" ../.VERSION; then
-        echo "$ID" >>"../dist/.VERSION"
+    if grep -q "$ID" ../VERSION.txt; then
+        echo "$ID" >>"../dist/VERSION.txt"
     else
         makepkg -sf --noconfirm
         pacman -U --noconfirm ./*.pkg.tar.zst
         mv ./*.pkg.tar.zst "../dist/$1.pkg.tar.zst"
-        echo "$ID" >>"../dist/.VERSION"
+        echo "$ID" >>"../dist/VERSION.txt"
     fi
 }
 
-wget -O ./.VERSION https://github.com/xgugugu/aur-packages/releases/download/x86_64/.VERSION
+wget -O ./VERSION.txt https://github.com/xgugugu/aur-packages/releases/download/x86_64/VERSION.txt
 mkdir dist
 cd dist && wget -O ./xgugugu.db.tar.gz https://github.com/xgugugu/aur-packages/releases/download/x86_64/xgugugu.db.tar.gz && cd ..
 
