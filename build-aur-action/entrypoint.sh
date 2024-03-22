1e6538318b08b1c8b00d39e62e75bb21e1df7552
咕咕 <xcz2034830996@163.com>
2024-03-22 12:11:22 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -5,19 +5,22 @@ build() {
     ID="$1@$HASH"
     if grep -q "$ID" ../VERSION.txt; then
         echo "$ID" >>"../dist/VERSION.txt" &&
-            echo "Checked $REPO. Not updated. Current version: $HASH" >>"../dist/LOGS.txt"
+            echo "[Not Updated] Checked $REPO. Current version: $HASH" >>"../dist/LOGS.txt"
     else
         makepkg -sf --noconfirm &&
             pacman -U --noconfirm ./*.pkg.tar.zst &&
             mv ./*.pkg.tar.zst "../dist/$1.pkg.tar.zst" &&
             echo "$ID" >>"../dist/VERSION.txt" &&
-            echo "Checked $REPO. Updated. Current version: $HASH" >>"../dist/LOGS.txt"
+            echo "[Updated] Checked $REPO. Current version: $HASH" >>"../dist/LOGS.txt"
+        if [ $? -ne 0 ]; then
+            echo "[Failed] Checked $REPO. Current version: $HASH" >>"../dist/LOGS.txt"
+        fi
     fi
 }
 
 wget -O ./VERSION.txt https://github.com/xgugugu/aur-packages/releases/download/x86_64/VERSION.txt
 wget -O ./dist/LOGS.txt https://github.com/xgugugu/aur-packages/releases/download/x86_64/LOGS.txt
-date +"Build %Y-%m-%d %H:%M:%S" >>"./dist/LOGS.txt"
+echo "Build $(date +"%Y-%m-%d %H:%M:%S")" >>"./dist/LOGS.txt"
 
 mkdir dist
 (cd dist && wget -O ./xgugugu.db.tar.gz https://github.com/xgugugu/aur-packages/releases/download/x86_64/xgugugu.db.tar.gz)
