59b0948c0685a7b6ab95e4a0d0ed892413bbe47c
咕咕 <xcz2034830996@163.com>
2024-06-13 08:27:33 +0800

    支持多包

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+renamef() {
+    cnt=0
+    for filename in "$."/*.pkg.tar.zst; do
+        mv "$filename" "../dist/${1}_${cnt}.pkg.tar.zst"
+        ((cnt++))
+    done
+}
+
 build() {
     HASH=$(git rev-parse --short HEAD)
     ID="$1@$HASH"
     if grep -q "$ID" ../VERSION.txt; then
         echo "$ID" >>"../dist/VERSION.txt"
     else
-        makepkg -sf --noconfirm &&
-            pacman -U --noconfirm ./*.pkg.tar.zst &&
-            mv ./*.pkg.tar.zst "../dist/$1.pkg.tar.zst" &&
-            echo "$ID" >>"../dist/VERSION.txt" &&
-            echo "[Updated] Checked $REPO. Current version: $HASH" >>"../dist/LOGS.txt"
+        makepkg -sf --noconfirm
+        pacman -U --noconfirm ./*.pkg.tar.zst
+        renamef "$1"
+        echo "$ID" >>"../dist/VERSION.txt"
+        echo "[Updated] Checked $REPO. Current version: $HASH" >>"../dist/LOGS.txt"
         if [ $? -ne 0 ]; then
             echo "[Failed] Checked $REPO. Current version: $HASH" >>"../dist/LOGS.txt"
         fi
@@ -25,9 +33,11 @@ mkdir dist
 wget -O ./dist/LOGS.txt https://github.com/xgugugu/aur-packages/releases/download/x86_64/LOGS.txt
 echo "Build $(date +"%Y-%m-%d %H:%M:%S")" >>"./dist/LOGS.txt"
 
+set -e
+
 for REPO in $1; do
-    git clone "https://aur.archlinux.org/$REPO.git" &&
-        (cd "$REPO" && build "$REPO") && rm -rf "$REPO"
+    git clone "https://aur.archlinux.org/$REPO.git"
+    (cd "$REPO" && build "$REPO") && rm -rf "$REPO"
 done
 
 repo-add "./dist/xgugugu.db.tar.gz" ./dist/*.pkg.tar.zst || true
